afc6b532fdb3aa306fb3e21dd5f087f388c4fbe2
liujiao <liujiao@cmiot.chinamobile.com>
2020-07-24 17:39:04 +0800

    springboot集成mybatis框架

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value = "com.example.demo.mapper")
+//MapperScan 自动扫描所有 Mybatis Mapper 文件，在这里扫描的是无 XML 形式的， XML形式的扫描与其大致相同。
 public class DemoApplication {
 
     public static void main(String[] args) {
